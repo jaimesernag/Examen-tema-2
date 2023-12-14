618273598f505393c0bda4faa87a2db17b3223c7
jaimesernag <jaime.sernagonzalez@gmail.com>
2023-12-14 13:07:28 +0100

    .

--- Ejercicio1c.cpp
+++ Ejercicio1c.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct Estudiante {
     char nombre[50];
@@ -18,3 +20,14 @@ int main() {
 
     return 0;
 }
+#define MAX_MATERIAS 10
+#define MAX_NOMBRE 50
+
+struct Estudiante {
+    char nombre[MAX_NOMBRE];
+    int edad;
+    float promedio;
+    char materias[MAX_MATERIAS][MAX_NOMBRE];
+    int cantidadMaterias;
+};
+
