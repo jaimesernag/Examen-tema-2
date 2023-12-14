3c9781ec5bf8ca0d90cc3fc3f1d11cb1979a8b58
jaimesernag <jaime.sernagonzalez@gmail.com>
2023-12-14 13:01:59 +0100

    .

--- Ejercicio1c.cpp
+++ Ejercicio1c.cpp
@@ -11,4 +11,10 @@ void mostrarEstudiante(struct Estudiante estudiante) {
     printf("Edad: %d\n", estudiante.edad);
     printf("Promedio: %.2f\n", estudiante.promedio);
 }
+int main() {
+    struct Estudiante alumno = {"Juan", 20, 85.5};
+    printf("Detalles del estudiante:\n");
+    mostrarEstudiante(alumno);
 
+    return 0;
+}
