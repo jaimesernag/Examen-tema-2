db0bea56bd244932b28c4eb5de298cac544f3a1d
jaimesernag <jaime.sernagonzalez@gmail.com>
2023-12-14 13:15:43 +0100

    .

--- Ejercicio1c++.cpp
+++ Ejercicio1c++.cpp
@@ -1,192 +1,100 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
 #include <stdexcept>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-struct Materia {
-    string nombre;
+class FechaInvalidaException : public exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: Formato de fecha incorrecto.";
+    }
 };
 
+
+class MateriaNoRegistradaException : public exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: Materia no registrada.";
+    }
+};
+
+
+struct Asistencia {
+    std::string fecha;
+    std::string materia;
+    std::string estado;
+};
+
+
+struct Materia {
+    std::string nombre;
+    float calificacion;
+};
+
+
 struct Estudiante {
-    string nombre;
+    std::string nombre;
     int edad;
     float promedio;
     vector<Materia> materias;
-};
-
-void mostrarMaterias(const Estudiante& estudiante) {
-    cout << "Materias inscritas por " << estudiante.nombre << ":" << endl;
-    for (const auto& materia : estudiante.materias) {
-        cout << "- " << materia.nombre << endl;
-    }
-}
-
-void agregarMateria(Estudiante& estudiante, const string& nombreMateria) {
-    Materia nuevaMateria {nombreMateria};
-    estudiante.materias.push_back(nuevaMateria);
-}
-
-void eliminarMateria(Estudiante& estudiante, const string& nombreMateria) {
-    estudiante.materias.erase(remove_if(estudiante.materias.begin(), estudiante.materias.end(),
-                                        [&nombreMateria](const Materia& materia) {
-                                            return materia.nombre == nombreMateria;
-                                        }), estudiante.materias.end());
-}
-
-int main() {
-    Estudiante estudiante {"Jaime", 19, 85.0f};
-    agregarMateria(estudiante, "Fisica");
-    agregarMateria(estudiante, "Historia");
-    mostrarMaterias(estudiante);
-
-    eliminarMateria(estudiante, "Historia");
-    cout << "\nDespués de eliminar la materia 'Historia':\n";
-    mostrarMaterias(estudiante);
-
-    return 0;
-}
-
-using namespace std;
-
-class Asistencia {
-private:
-    string fecha;
-    string materia;
-    string estado;
-
-public:
-    Asistencia(string fecha, string materia, string estado)
-            : fecha(fecha), materia(materia), estado(estado) {}
-
-    void mostrarAsistencia() {
-        cout << "Fecha: " << fecha << endl;
-        cout << "Materia: " << materia << endl;
-        cout << "Estado: " << estado << endl;
-    }
-};
-
-class Estudiante {
-private:
-    string nombre;
-    int edad;
-    float promedio;
     vector<Asistencia> asistencias;
 
-public:
-    Estudiante(string nombre, int edad, float promedio)
-            : nombre(nombre), edad(edad), promedio(promedio) {}
 
-    void registrarAsistencia(string fecha, string materia, string estado) {
-        Asistencia nuevaAsistencia(fecha, materia, estado);
-        asistencias.push_back(nuevaAsistencia);
+    void registrarAsistencia(const std::string& fecha, const std::string& materia, const std::string& estado) {
+
+        if (fecha.size() != 10 || fecha[4] != '-' || fecha[7] != '-') {
+            throw FechaInvalidaException();
+        }
+
+
+        if (std::find_if(materias.begin(), materias.end(),
+                         [&](const Materia& mat) { return mat.nombre == materia; }) == materias.end()) {
+            throw MateriaNoRegistradaException();
+        }
+
+
+        asistencias.push_back({fecha, materia, estado});
     }
 
-    void mostrarAsistencias() {
-        cout << "Asistencias de " << nombre << ":\n";
+
+    void mostrarInformacion() const {
+        std::cout << "Informacion del Estudiante:" << std::endl;
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << std::endl;
+        std::cout << "Promedio: " << promedio << std::endl;
+
+        std::cout << "\nMaterias Inscritas:" << std::endl;
+        for (const auto& materia : materias) {
+            std::cout << "- " << materia.nombre << " (Calificacion: " << materia.calificacion << ")" << std::endl;
+        }
+
+        std::cout << "\nAsistencias Registradas:" << std::endl;
         for (const auto& asistencia : asistencias) {
-            asistencia.mostrarAsistencia();
-            cout << endl;
+            std::cout << "- Fecha: " << asistencia.fecha << ", Materia: " << asistencia.materia << ", Estado: " << asistencia.estado << std::endl;
         }
     }
 };
 
 int main() {
-    Estudiante estudiante("Juan", 20, 85.5f);
-    estudiante.registrarAsistencia("2023-12-01", "Fisica", "Asistió");
-    estudiante.registrarAsistencia("2023-12-03", "Historia", "Falta");
-
-    estudiante.mostrarAsistencias();
-
-    return 0;
-}
-
-using namespace std;
-
-// Excepción personalizada para manejar errores en el formato de la fecha
-class FormatoFechaInvalido : public exception {
-public:
-    const char* what() const noexcept override {
-        return "Error: Formato de fecha inválido.";
-    }
-};
-
-// Excepción personalizada para manejar materias no registradas
-class MateriaNoRegistrada : public exception {
-private:
-    string nombreMateria;
-
-public:
-    MateriaNoRegistrada(const string& nombre) : nombreMateria(nombre) {}
-
-    const char* what() const noexcept override {
-        return ("Error: La materia '" + nombreMateria + "' no está registrada.").c_str();
-    }
-};
-
-class Asistencia {
-private:
-    string fecha;
-    string materia;
-    string estado;
-
-public:
-    Asistencia(string fecha, string materia, string estado)
-            : fecha(fecha), materia(materia), estado(estado) {}
-
-    void mostrarAsistencia() const {
-        cout << "Fecha: " << fecha << endl;
-        cout << "Materia: " << materia << endl;
-        cout << "Estado: " << estado << endl;
-    }
-};
-
-class Estudiante {
-private:
-    string nombre;
-    vector<string> materiasRegistradas;
-    vector<Asistencia> asistencias;
-
-public:
-    Estudiante(string nombre, const vector<string>& materias)
-            : nombre(nombre), materiasRegistradas(materias) {}
-
-    void registrarAsistencia(string fecha, string materia, string estado) {
-        // Verificar si la materia está registrada
-        if (find(materiasRegistradas.begin(), materiasRegistradas.end(), materia) == materiasRegistradas.end()) {
-            throw MateriaNoRegistrada(materia);
-        }
-
-        // Aquí puedes agregar la lógica para verificar el formato de la fecha si lo deseas
-
-        // Si todo está bien, registrar la asistencia
-        Asistencia nuevaAsistencia(fecha, materia, estado);
-        asistencias.push_back(nuevaAsistencia);
-    }
-
-    void mostrarAsistencias() const {
-        cout << "Asistencias de " << nombre << ":\n";
-        for (const auto& asistencia : asistencias) {
-            asistencia.mostrarAsistencia();
-            cout << endl;
-        }
-    }
-};
-
-int main() {
-    vector<string> materiasRegistradas = {"Fisica", "Historia", "Matematicas"};
-    Estudiante estudiante("Juan", materiasRegistradas);
-
     try {
-        estudiante.registrarAsistencia("2023-12-01", "Fisica", "Asistió");
-        estudiante.registrarAsistencia("2023-12-03", "Geografía", "Falta"); // Intentando registrar una materia no registrada
-    } catch (const MateriaNoRegistrada& e) {
-        cout << "Excepción capturada: " << e.what() << endl;
-    }
+        Estudiante estudiante1 = {"Javier", 18, 9.1};
 
-    estudiante.mostrarAsistencias();
+
+        estudiante1.materias.push_back({"Matematicas", 9.0});
+        estudiante1.materias.push_back({"Historia", 8.0});
+
+
+        estudiante1.registrarAsistencia("2023/01/01", "Matematicas", "asistio");
+
+
+        estudiante1.registrarAsistencia("2023-01-02", "Geografia", "asistio");
+
+
+        estudiante1.mostrarInformacion();
+    } catch (const std::exception& e) {
+        std::cerr << "Excepción: " << e.what() << std::endl;
+    }
 
     return 0;
 }
