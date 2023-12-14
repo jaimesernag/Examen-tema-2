c0245e1120c3cd626e2c8d77d042e15f5b2c85b8
jaimesernag <jaime.sernagonzalez@gmail.com>
2023-12-14 13:01:12 +0100

    .

---
+++ Ejercicio1c.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+void mostrarEstudiante(struct Estudiante estudiante) {
+    printf("Nombre: %s\n", estudiante.nombre);
+    printf("Edad: %d\n", estudiante.edad);
+    printf("Promedio: %.2f\n", estudiante.promedio);
+}
+
