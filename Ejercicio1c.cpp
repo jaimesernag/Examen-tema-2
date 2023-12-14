88b8b31d85ab411e81a2ffedf3e99f55ada3a561
jaimesernag <jaime.sernagonzalez@gmail.com>
2023-12-14 13:10:24 +0100

    .

--- Ejercicio1c.cpp
+++ Ejercicio1c.cpp
@@ -42,4 +42,42 @@ void mostrarMaterias(struct Estudiante estudiante) {
         }
     }
 }
+void agregarMateria(struct Estudiante *estudiante, const char *nombreMateria) {
+    if (estudiante->cantidadMaterias < MAX_MATERIAS) {
+        strcpy(estudiante->materias[estudiante->cantidadMaterias], nombreMateria);
+        estudiante->cantidadMaterias++;
+    } else {
+        printf("El estudiante ha alcanzado el límite de materias.\n");
+    }
+}
 
+// Función para eliminar una materia del estudiante
+void eliminarMateria(struct Estudiante *estudiante, const char *nombreMateria) {
+    for (int i = 0; i < estudiante->cantidadMaterias; i++) {
+        if (strcmp(estudiante->materias[i], nombreMateria) == 0) {
+            for (int j = i; j < estudiante->cantidadMaterias - 1; j++) {
+                strcpy(estudiante->materias[j], estudiante->materias[j + 1]);
+            }
+            estudiante->cantidadMaterias--;
+            break;
+        }
+    }
+}
+
+int main() {
+    struct Estudiante alumno = {"Juan", 20, 85.5, {{"Matemáticas"}, {"Física"}, {"Historia"}}, 3};
+
+    mostrarMaterias(alumno);
+
+    agregarMateria(&alumno, "Química");
+
+    printf("\nDespués de agregar una materia:\n");
+    mostrarMaterias(alumno);
+
+    eliminarMateria(&alumno, "Física");
+
+    printf("\nDespués de eliminar una materia:\n");
+    mostrarMaterias(alumno);
+
+    return 0;
+}
